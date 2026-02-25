32a4632a1fc734c05c74ac027c30929527e76118
theKiess <frank_kiess.junior@hotmail.com>
2026-02-24 21:53:31 -0300

    Resto da primeira aula.

--- aulas/aula1/p04.cpp
+++ aulas/aula1/p04.cpp
@@ -6,7 +6,16 @@ using namespace std;
 
 int main()
 {
+  ofstream dsTab;
+  dsTab.open("tabela.txt");
 
+  for (int i = 0; i < 100; i++)
+    // dsTab << i << endl;
+    dsTab << dec << i         << " "
+          << hex << uppercase << i
+          << endl; 
+
+  dsTab.close(); 
 
   return 0;
 }
